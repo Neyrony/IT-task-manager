f231ee2b4cc7f7eef0388cd2d248c442d9ce64c1
Neyrony <sserebriy@gmail.com>
2026-07-12 12:14:31 +0300

    feat: change main page with new view and template & add new pagination block in base.html

--- manager/urls.py
+++ manager/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from manager.views import TaskListView
+from manager.views import TaskListView, main_page
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="task_list"),
+    path("", main_page, name="index"),
+    path("tasks/", TaskListView.as_view(), name="task_list"),
 ]
 
 app_name = "manager"
--- manager/views.py
+++ manager/views.py
@@ -1,8 +1,21 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 from django.views.generic import ListView
 
 from manager.models import Task
 
 
+def main_page(request: HttpRequest) -> HttpResponse:
+    visits_amount = request.session.get("visits_amount", 0) + 1
+    request.session["visits_amount"] = visits_amount
+    context = {"visits_amount": visits_amount}
+    return render(request, "manager/index.html", context=context)
+
+
 class TaskListView(ListView):
     model = Task
     paginate_by = 10
+
+    def get_queryset(self):
+        return Task.objects.select_related("task_type")
+
