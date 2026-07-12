f40e60647a081f47a7fa27ee075e3dad186484f1
Neyrony <sserebriy@gmail.com>
2026-07-12 16:04:01 +0300

    feat: add crispy forms

--- it_task_manager/settings.py
+++ it_task_manager/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "manager",
 ]
 
@@ -125,3 +127,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
