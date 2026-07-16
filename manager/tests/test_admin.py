b8d35d3a21db13e4f9ce13f108ed7fc531a97d4d
Neyrony <sserebriy@gmail.com>
2026-07-16 18:21:53 +0300

    test: extract base authorization class to test_base.py

--- manager/tests/test_admin.py
+++ manager/tests/test_admin.py
@@ -1,24 +1,9 @@
 import datetime
 
-from django.contrib.auth import get_user_model
-from django.test import TestCase, Client
 from django.urls import reverse
 
 from manager.models import Position, TaskType, Task
-
-
-class ClientAuthorization(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.admin = get_user_model().objects.create_superuser(
-            username="admin",
-            password="test_password",
-            first_name="Admin",
-            last_name="Test",
-            email="admin@test.com",
-            position=Position.objects.create(name="test1"),
-        )
-        self.client.force_login(self.admin)
+from manager.tests.test_base import ClientAuthorization
 
 
 class TestPositionAdmin(ClientAuthorization):
---
+++ manager/tests/test_base.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+
+from manager.models import Position
+
+
+class ClientAuthorization(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin = get_user_model().objects.create_superuser(
+            username="admin",
+            password="test_password",
+            first_name="Admin",
+            last_name="Test",
+            email="admin@test.com",
+            position=Position.objects.create(name="test1"),
+        )
+        self.client.force_login(self.admin)
